d2086a9143e0134511397f95e31705b3dc4a04c8
Anders Pearson <anders@columbia.edu>
2013-08-25 13:52:57 -0400

    sedgewick's more concise version

--- algorithms/week2/binarysearch/binarysearch.go
+++ algorithms/week2/binarysearch/binarysearch.go
@@ -3,28 +3,20 @@ package binarysearch
 const notfound = -1
 
 func search(a []int, v int) int {
-	if len(a) == 0 {
-		return notfound
-	}
 	lo := 0
 	hi := len(a) - 1
-	mid := ((hi - lo) / 2) + lo
 
-	for lo != hi {
-		if a[mid] == v {
-			return mid
-		}
+	for lo <= hi {
+		mid := ((hi - lo) / 2) + lo
 		if v < a[mid] {
 			// left
 			hi = mid - 1
-		} else {
+		} else if v > a[mid] {
 			// right
 			lo = mid + 1
+		} else {
+			return mid
 		}
-		mid = ((hi - lo) / 2) + lo
-	}
-	if a[mid] == v {
-		return mid
 	}
 	// if we reach the end, we know we are done
 	return notfound
